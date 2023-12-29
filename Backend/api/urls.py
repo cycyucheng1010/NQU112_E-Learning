af288c9e63092ecb7dd16ff30248e7573a807e05
Rick Chen <rick20001010@gmail.com>
2023-12-29 14:59:51 +0800

    20231229

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProjectViewset, UserViewset,EnglishWordSearchAPIView
+from .views import ProjectViewset, UserViewset#,EnglishWordSearchAPIView
 from rest_framework.routers import DefaultRouter
 from .function.exam import ExamViewset
 
@@ -7,7 +7,7 @@ router = DefaultRouter()
 router.register('project',ProjectViewset,basename='project')
 router.register('user', UserViewset, basename='user')
 router.register('exam', ExamViewset, basename='exam')
-router.register('search',EnglishWordSearchAPIView,basename='search')
+#router.register('search',EnglishWordSearchAPIView,basename='search')
 urlpatterns = router.urls
 
 # urlpatterns =[
