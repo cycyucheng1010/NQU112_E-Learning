ede89ca72993af30e6f5f45c78da8d0eff268f0e
nelson023 <s110190523@student.nqu.edu.tw>
2023-12-31 17:29:55 +0800

    nelson added

--- Backend/api/function/sd.py
+++ Backend/api/function/sd.py
@@ -65,3 +65,134 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+from openai import OpenAI
+
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.decorators import action
+import traceback
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework import viewsets
+from django.http import HttpResponse, JsonResponse
+import os
+'''
+client = OpenAI(
+    api_key=os.environ.get("sk-n2NyHG6Z2CUdEY3CeORAT3BlbkFJ2WbBGga2RuHTvGMZtVN"),
+)'''
+client = OpenAI(api_key="sk-n2NyHG6Z2CUdEY3CeORAT3BlbkFJ2WbBGga2RuHTvGMZtVN")
+
+#class GPTView(APIView): 
+class GPTView(viewsets.ViewSet): 
+    @csrf_exempt
+    @action(detail=False, methods=['GET','POST'], url_path='gpt')
+    def gpt (self, request):
+        if request.method =='POST':
+            try:
+                list = []
+                response = client.chat.completions.create(
+                    model="gpt-3.5-turbo", 
+                    messages=[
+                        {"role": "system", "content": "You are a English teacher."},
+                        {"role": "user", "content": "Craft a vivid sentence focusing on 'tea,' ensuring a clear understanding of its meaning (within 15 words)."}
+                    ],
+                    max_tokens=30
+                )
+                
+                data = response.choices[0].message.content
+                sentence_list.append(data)
+
+                # Print the sentence and output to sentence_input
+                print(f"Sentence: {data}")
+                sentence_input = data  # 賦值給 sentence_input 或進行其他處理
+
+                return JsonResponse({"msg": "success", "data": sentence_list})
+
+            except Exception as e:
+                traceback.print_exc()
+                return JsonResponse({"msg": "error"})
+
+#gpt_view = GPTView.as_view()
+
+
+
+@api_view(['POST'])
+def generate_sentence(request):
+    data = request.data  # 使用 request.data 可以直接解析 JSON 數據
+    word_input = data.get('word')
+
+    sentence_input = data.get('sentence')
+
+
+
+    return JsonResponse({"status": "success", "sentence": f"((({word_input})))"})
+
+
+@api_view(['POST'])
+def generate_image(request):
+    data = json.loads(request.body)
+    word_input = data.get('word')
+    sentence_input = data.get('sentence')
+
+    wordpic_folder = "./function/wordpic"
+    os.makedirs(wordpic_folder, exist_ok=True)
+    image_filename = os.path.join(wordpic_folder, f"{word_input}.png")
+
+    if os.path.exists(image_filename):
+        print(f"圖片 '{image_filename}' 已經存在，無需再次生成。")
+        return JsonResponse({"status": "success", "image_url": f"{word_input}.png"})
+
+    prompt = f"{sentence_input.replace(word_input, f'((({word_input})))')}"
+    response = send_api_request(prompt)
+
+    if response and response.status_code == 200:
+        image_url = extract_image_url(response)
+
+        if image_url:
+            download_image(image_url, image_filename)
+            return JsonResponse({"status": "success", "image_url": f"{word_input}.png"})
+
+    return JsonResponse({"status": "error", "message": "生成失敗"})
+
+def send_api_request(prompt):
+    url = "https://stablediffusionapi.com/api/v3/text2img"
+    payload = {
+        "key": "kRdhAtCe7TqcTgUkpoBeWB569nwAO7UnvR3BGvVGBj2zJtKbsapxWka0sPQ2",
+        "prompt": prompt,
+        "width": "512",
+        "height": "512",
+        "samples": "1",
+        "num_inference_steps": "20",
+        "guidance_scale": 7.5,
+        "safety_checker": "yes",
+        "multi_lingual": "no",
+        "panorama": "no",
+        "self_attention": "no",
+        "upscale": "no",
+        "embeddings_model": None,
+        "webhook": None,
+        "track_id": None
+    }
+
+    headers = {'Content-Type': 'application/json'}
+
+    return post(url, headers=headers, json=payload)
+
+def extract_image_url(response):
+    response_data = response.json()
+    return response_data.get("output")[0] if "output" in response_data else None
+
+def download_image(image_url, image_filename):
+    response_image = get(image_url)
+    with open(image_filename, 'wb') as img_file:
+        img_file.write(response_image.content)
+
---
+++ Backend/api/function/tt.py
@@ -0,0 +1,125 @@
+import os
+import traceback
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from openai import OpenAI
+from requests import post, get  # 导入 post 和 get 模块
+
+# 初始化 OpenAI 客户端
+client = OpenAI(api_key="sk-n2NyHG6Z2CUdEY3CeORAT3BlbkFJ2WbBGga2RuHTvGMZtVN")
+
+# 定义文件路径
+word_sentence_folder = "./function/wordsentence"
+word_pic_folder = "./function/wordpic"
+
+# 确保目录存在
+os.makedirs(word_sentence_folder, exist_ok=True)
+os.makedirs(word_pic_folder, exist_ok=True)
+
+@csrf_exempt
+@api_view(['POST'])
+def generate_sentence(request):
+    try:
+        sentence_list = []
+
+        # 从请求数据中获取单词和句子
+        data = request.data
+        word_input = data.get('word')
+        sentence_input = data.get('sentence')
+
+        # 使用 OpenAI API 生成句子
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo", 
+            messages=[
+                {"role": "system", "content": "You are an English teacher."},
+                {"role": "user", "content": f"Craft a vivid sentence focusing on '{word_input}', ensuring a clear understanding of its meaning (within 15 words)."}
+            ],
+            max_tokens=30
+        )
+        
+        generated_sentence = response.choices[0].message.content
+        sentence_list.append(generated_sentence)
+
+        # 保存生成的句子到文件
+        sentence_filename = os.path.join(word_sentence_folder, f"{word_input}.txt")
+        with open(sentence_filename, 'w') as file:
+            file.write(generated_sentence)
+
+        # 输出生成的句子并返回给前端
+        print(f"Generated Sentence: {generated_sentence}")
+        
+        return JsonResponse({"status": "success", "sentence": generated_sentence})
+
+    except Exception as e:
+        traceback.print_exc()
+        return JsonResponse({"status": "error"})
+
+# 另外的函数，如果有的话
+@api_view(['POST'])
+def another_function(request):
+    # 其他逻辑
+    return JsonResponse({"status": "success"})
+
+@api_view(['POST'])
+def generate_image(request):
+    try:
+        data = json.loads(request.body)
+        word_input = data.get('word')
+        sentence_input = data.get('sentence')
+
+        image_filename = os.path.join(word_pic_folder, f"{word_input}.png")
+
+        if os.path.exists(image_filename):
+            print(f"圖片 '{image_filename}' 已經存在，無需再次生成。")
+            return JsonResponse({"status": "success", "image_url": f"{word_input}.png"})
+
+        prompt = f"{sentence_input.replace(word_input, f'((({word_input})))')}"
+        response = send_api_request(prompt)
+
+        if response and response.status_code == 200:
+            image_url = extract_image_url(response)
+
+            if image_url:
+                download_image(image_url, image_filename)
+                return JsonResponse({"status": "success", "image_url": f"{word_input}.png"})
+
+        return JsonResponse({"status": "error", "message": "生成失敗"})
+
+    except Exception as e:
+        traceback.print_exc()
+        return JsonResponse({"status": "error", "message": str(e)})
+
+def send_api_request(prompt):
+    url = "https://stablediffusionapi.com/api/v3/text2img"
+    payload = {
+        "key": "kRdhAtCe7TqcTgUkpoBeWB569nwAO7UnvR3BGvVGBj2zJtKbsapxWka0sPQ2",
+        "prompt": prompt,
+        "width": "512",
+        "height": "512",
+        "samples": "1",
+        "num_inference_steps": "20",
+        "guidance_scale": 7.5,
+        "safety_checker": "yes",
+        "multi_lingual": "no",
+        "panorama": "no",
+        "self_attention": "no",
+        "upscale": "no",
+        "embeddings_model": None,
+        "webhook": None,
+        "track_id": None
+    }
+
+    headers = {'Content-Type': 'application/json'}
+
+    return post(url, headers=headers, json=payload)
+
+def extract_image_url(response):
+    response_data = response.json()
+    return response_data.get("output")[0] if "output" in response_data else None
+
+def download_image(image_url, image_filename):
+    response_image = get(image_url)
+    with open(image_filename, 'wb') as img_file:
+        img_file.write(response_image.content)
