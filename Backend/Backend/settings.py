41119dd228b689beed4826bbf08c54de7b71dd94
cjlee <cjlee@mail.com>
2024-01-17 00:41:24 +0800

    test

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -53,9 +53,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-]
+#CORS_ALLOWED_ORIGINS = [
+#    'http://localhost:3000'
+#]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
